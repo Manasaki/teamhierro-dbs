c544ea8dd7816adcb9872abc74e13a78a6669802
Lasana Milton, Jr <lasanamilton@hotmail.com>
2017-01-30 10:47:06 -0500

    Add files via upload
    Current configuration on Ubuntu Test-server. Used for local testing before updating on live-server.
    
    http://lasanamilton.ddns.net/dbs/

---
+++ mysql/index.php
@@ -0,0 +1,3 @@
+<?php
+	echo "<center><p>You do not have permissions for this page. <br/> Please go <a href= 'http://lasanamilton.ddns.net/dbs/'>BACK</a></p></center>";
+?>
---
+++ resources/media/mkv/index.php
@@ -0,0 +1,3 @@
+<?php
+	echo "<center><p>You do not have permissions for this page. <br/> Please go <a href= '../'>BACK</a></p></center>";
+?>
